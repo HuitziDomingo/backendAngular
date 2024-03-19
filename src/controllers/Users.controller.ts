38100c409e815bc68fc5cd0efe8e3e0aef652f26
HuitziDomingo <false>
2024-03-18 20:42:53 -0600

    validacion de campos principales y mensaje de error para el cliente

--- src/controllers/Users.controller.ts
+++ src/controllers/Users.controller.ts
@@ -4,6 +4,7 @@ import { Errors } from "../utils/error.handle"
 import UserModel from "../models/User"
 import { UsersInterface } from "../interfaces/Users.interface"
 
+
 export class Users {
 
     static async getUsers(req: Request, res: Response) {
@@ -16,20 +17,19 @@ export class Users {
     }
 
 
-    static async createUser({ body }: Request, res: Response) {
+    static async createUser(req: Request, res: Response) {
         try {
-            let userExist = await UserModel.findOne<UsersInterface>({email: body.email})
-            if (userExist) 
-                return Errors.handleHTTP(res, 'USUARIO_YA_EXISTE')
+            let userExist = await UserModel.findOne<UsersInterface>({ email: req.body.email })
+            if (userExist)
+                return Errors.handleHTTP(res, 'El usuario ya existe.')
             else {
-                let user = await User.createUsers(body)
+                let user = await User.createUsers(req.body)
                 return res.send(user)
             }
 
         } catch (error) {
-            return Errors.handleHTTP(res, 'ERROR_CREATE_USER', error)
+            return Errors.handleHTTP(res, 'ERROR al crear el usuario.', error)
         }
-
     }
 
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,9 +1,16 @@
 import { Router } from "express"
 import { Users } from '../controllers/Users.controller'
+import { check } from "express-validator"
+import { ValidateFields } from "../middlewares/validate-fields"
 
 const router = Router()
 
 router.get('/users', Users.getUsers)
-router.post('/users', Users.createUser)
+router.post('/users', [
+    check('name', "El nombre es obligatorio.").not().isEmpty(),
+    check('password', "El password es obligatorio.").not().isEmpty(),
+    check('email', "El email es obligatorio.").isEmail(),
+    ValidateFields.validateFields
+],Users.createUser)
 
 export default router
